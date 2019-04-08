e7df7f6f3928142a8a0c1af4874e9da87aed8c1b
pyschedelicsid <pyschedelicsid@gmail.com>
2019-04-08 14:22:47 +0530

    Function to test ramg

---
+++ code-sidd/ramg_solver/test_ramg.m
@@ -0,0 +1,14 @@
+% Testing, Recursive V-cycle Algebraic Multigrid Method on BA problem
+
+function test_ramg()
+
+	load JTJ.dat            % loading approximate hessian matrix csr format
+
+    A = spconvert(JTJ);     % converting the data in matrix form
+    [m,n] = size(A);        % finding the size of JTJ matrix
+    b = rand(m,1);          % taking right hand side b to mx1 vector
+
+	[x, vc_cnt, rel_res] = ramg(A, b, @GS_Iter, 1, 1, 1e-10, 20);
+
+end
+
