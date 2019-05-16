09f6aef48a505898d51991faa1c29dbf44283f08
pyschedelicsid <pyschedelicsid@gmail.com>
2019-05-16 21:22:17 +0530

    changes to ramg

--- codes/code-sidd/ramg_solver/test_ramg.m
+++ codes/code-sidd/ramg_solver/test_ramg.m
@@ -5,8 +5,8 @@ function test_ramg()
 	load JTJ.dat            % loading approximate hessian matrix csr format
 
    	A = spconvert(JTJ);     % converting the data in matrix form
-    	[m,n] = size(A);        % finding the size of JTJ matrix
-    	b = rand(m,1);          % taking right hand side b to mx1 vector
+    [m,n] = size(A);        % finding the size of JTJ matrix
+    b = rand(m,1);          % taking right hand side b to mx1 vector
 
 	[x, vc_cnt, rel_res] = ramg(A, b, @GS_Iter, 1, 1, 1e-10, 20);
 
