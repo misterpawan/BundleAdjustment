39c74cb50f881d5cf4496845f2811d9bd6440fc7
Shrutimoy Das <shrutimoy.das@ada.iiit.ac.in>
2019-09-09 13:34:04 +0530

    Automation done

--- codes/SSBA-4.0/run_make.sh
+++ codes/SSBA-4.0/run_make.sh
@@ -10,10 +10,13 @@ clear
 
 cd Apps
 
+DATA_FILE=~/Dataset/problem-308-195089-pre.txt
+PARAM_FILE=~/BundleAdjustment/codes/SSBA-4.0/params.txt
+
 
 #USAGE:
 # ./bundle_large_lifted_schur <sparse reconstruction file> <parameter file>
 # params.txt : MAX_GMRES_ITERATIONS RESTARTS_FOR_GMRES TOLERANCE
-./bundle_large_lifted_schur ~/Dataset/problem-138-19878-pre.txt ~/BundleAdjustment/codes/SSBA-4.0/params.txt
+./bundle_large_lifted_schur  $DATA_FILE $PARAM_FILE
 #./bundle_large_lifted_schur ~/Dataset/problem-49-7776-pre.txt
 #./bundle_large ~/Dataset/problem-49-7776-pre.txt
